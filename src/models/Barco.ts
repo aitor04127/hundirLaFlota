9c4bd64716dfad14f7e16fc57dbc39a8734f4218
asintota <seniorbot678@gmail.com>
2024-11-21 09:40:08 +0100

    metodo posicionar barco

--- src/models/Barco.ts
+++ src/models/Barco.ts
@@ -9,4 +9,24 @@ constructor(tamano:number,tocado:number,posicionbarco:number, hundido:boolean){
     this.tocado=tocado;
     this.posicionbarco = new Set<number>();
     this.hundido=hundido;
-}}
+}
+  
+  posicionarbarco(inicio: number, direccion: 'horizontal' | 'vertical', tamañoTablero: number): boolean {
+   
+    for (let i = 0; i < this.tamano; i++) {
+        let posicion;
+        if (direccion === 'horizontal') {
+            posicion = inicio + i; 
+        } else {
+            posicion = inicio + i * tamañoTablero; 
+        }
+
+        
+        if (posicion < 0 || posicion >= tamañoTablero * tamañoTablero) {
+            return false; 
+        }
+        this.posicionbarco.add(posicion);
+    }
+    return true; 
+}
+}
