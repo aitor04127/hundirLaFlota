7b0a5b67bbe2df07968aa965dbbbfac4ce658ba0
PRIET019 <prietosuarezpablo@gmail.com>
2024-11-28 08:22:03 +0100

    printar disparos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Casilla } from '../models/Casilla'
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,29 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  tablero: Casilla[][] = [[new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla],
+                          [new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla],
+                          [new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla],
+                          [new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla],
+                          [new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla],
+                          [new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla],
+                          [new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla],
+                          [new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla],
+                          [new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla],
+                          [new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla,new Casilla, new Casilla]];
   title = 'Hundir_La_Flota';
+
+
+
+
+
+
+
+
+
+
+  disparar(x: number,y: number) {
+    this.tablero[x][y].url = "\\img\\tocado.png";
+    console.log("X: " + x + " Y: " + y);
+  }
 }
