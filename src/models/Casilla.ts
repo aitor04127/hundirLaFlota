c6083c4f6b42773002abfa407c157e6f2698a8a5
Aitor <aitorflorestecnojrj2@gmail.com>
2024-11-28 08:40:33 +0100

    imagenes y models/Casilla.ts

---
+++ src/models/Casilla.ts
@@ -0,0 +1,11 @@
+export class Casilla{
+    disparado:boolean;
+    tocado:boolean;
+    url:string;
+ 
+    constructor (){
+        this.disparado=false;
+        this.tocado=false;
+        this.url="img\\mar.png";
+        }
+}
